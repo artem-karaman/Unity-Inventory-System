92c05707df42ba8e7121ac3056f14886e3f7a4ac
Artem <karaman55_09@mail.ru>
2020-11-06 18:39:21 +0600

    add simple DragAndDrop and ItemSlot script

---
+++ Unity-Inventory-System/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour , IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("On Drop");
+
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition =
+                GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
