a6b02d36aa1bb5184b7f9f90c7d04312a38e4401
Artem <karaman55_09@mail.ru>
2020-11-07 00:19:40 +0600

    add SlotBehaviour

---
+++ Unity-Inventory-System/Assets/Scripts/Inventory/SlotBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace UnityInventorySystem.Inventory
+{
+	public class SlotBehaviour : MonoBehaviour, IPoolable<IMemoryPool>, IDisposable
+	{
+		private IMemoryPool _memoryPool;
+		
+		public void OnDespawned()
+		{
+			
+		}
+
+		public void OnSpawned(IMemoryPool memoryPool)
+		{
+			_memoryPool = memoryPool;
+		}
+
+		public void Dispose()
+		{
+			_memoryPool.Despawn(this);
+		}
+
+		public class Factory : PlaceholderFactory<SlotBehaviour>{}
+	}
+}
