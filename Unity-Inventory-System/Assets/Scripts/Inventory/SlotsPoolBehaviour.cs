53674d8787e61a58e7d9198a5f056df23e7f2010
Artem <karaman55_09@mail.ru>
2020-11-07 00:19:56 +0600

    add SlotPoolBehaviour

---
+++ Unity-Inventory-System/Assets/Scripts/Inventory/SlotsPoolBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UnityInventorySystem.Inventory
+{
+	public class SlotsPoolBehaviour : MonoBehaviour
+	{
+		private readonly SlotBehaviour.Factory _slotBehaviourFactory;
+		private readonly List<SlotBehaviour> _slots = new List<SlotBehaviour>();
+
+		public SlotsPoolBehaviour(
+			SlotBehaviour.Factory slotBehaviourFactory)
+		{
+			_slotBehaviourFactory = slotBehaviourFactory;
+		}
+
+		public void AddSlot()
+		{
+			var slot = _slotBehaviourFactory.Create();
+			_slots.Add(slot);
+		}
+
+		public void RemoveSlot()
+		{
+			if (_slots.Any())
+			{
+				var slot = _slots[0];
+				slot.Dispose();
+				_slots.Remove(slot);
+			}
+		}
+	}
+}
